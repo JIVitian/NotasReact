d6abc257c67b90524b17803bbbf8814551ea1f1e
JIVitian <jivitian@gmail.com>
2021-10-19 22:06:49 -0300

    Probando Custom Hooks

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  // Datos de la peticion
+  const [data, setData] = useState(null);
+  // Estado de la peticion
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    (async (url) => {
+      try {
+        let res = await fetch(url);
+        if (!res.ok) {
+          throw {
+            err: true,
+            status: res.status,
+            statusText: res.statusText ? res.statusText : "Ocurrio un error",
+          };
+        }
+
+        let data = await res.json();
+
+        setIsPending(false);
+        setData(data);
+        setError({err:false});
+
+
+      } catch (err) {
+        setIsPending(true);
+        setError(err);
+      }
+      })(url);
+  }, [url]);
+
+  return { data, isPending, error };
+};
